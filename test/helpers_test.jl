101dd199482a2eb2eada669e47e6e78714e09739
Albert Gnadt <albert.gnadt@gmail.com>
2020-10-19 19:11:47 -0400

    Removed unnecessary unit tests, all passing

--- test/helpers_test.jl
+++ test/helpers_test.jl
@@ -1,5 +1,10 @@
 using MagNav, Test, MAT
 
+test_file = "test_data_TL.mat"
+TL_data = matopen(test_file,"r") do file
+    read(file,"TL_data")
+end
+
 test_file = "test_data_traj.mat"
 traj = matopen(test_file,"r") do file
     read(file,"traj")
@@ -10,10 +15,8 @@ map_data = matopen(test_file,"r") do file
     read(file,"map_data")
 end
 
-test_file = "test_data_grid.mat"
-grid_data = matopen(test_file,"r") do file
-    read(file,"grid_data")
-end
+mag_cor   = TL_data["mag_cor"]
+mag_cor_d = TL_data["mag_cor_d"]
 
 lat = traj["lat"][1]
 lon = traj["lon"][1]
@@ -24,6 +27,11 @@ map_yy  = vec(map_data["yy"])
 
 interp_mapS = gen_interp_map(map_map,map_xx,map_yy)
 
-@testset "Interp Map Tests" begin
-    @test interp_mapS(lon,lat) ≈ grid_data["interp_map"]
+@testset "Detrend Tests" begin
+    @test detrend(mag_cor) ≈ mag_cor_d
+    @test detrend([3,6,8]) ≈ [-1,2,-1] / 6
+end
+
+@testset "Map Grad Tests" begin
+    @test map_grad(interp_mapS,lon,lat) ≈ [646.2163302216359,13720.07531468666]
 end
