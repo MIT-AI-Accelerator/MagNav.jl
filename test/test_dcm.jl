e5b3904fb4f4fa469e54595ae9fdcd879f3dfea6
Albert Gnadt <albert.gnadt@gmail.com>
2022-08-21 14:31:54 -0500

    Minor updates

--- test/test_dcm.jl
+++ test/test_dcm.jl
@@ -25,6 +25,7 @@ Cbn_temp = euler2dcm(rpy[1],rpy[2],rpy[3],:nav2body)
     @test Cnb            ≈ dcm_data["Cnb"]
     @test Cnb_estimate_1 ≈ dcm_data["Cnb_estimate"][:,:,1]
     @test Cnb_estimate   ≈ dcm_data["Cnb_estimate"]
+    @test_nowarn correct_Cnb(Cnb_1,zeros(3,1))
 end
 
 @testset "euler2dcm & dcm2euler tests" begin
