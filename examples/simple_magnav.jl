72a9f959d318234e6a4d6660fa8062a5464dc0a9
Albert Gnadt <20693045+gnadt@users.noreply.github.com>
2025-02-22 11:58:33 -0500

    Second pass at simple MagNav example

--- examples/simple_magnav.jl
+++ examples/simple_magnav.jl
@@ -1,23 +1,25 @@
-##*  Simple MagNav example to use with YOUR data
+##*  This is a simple MagNav example to use with YOUR data from scratch
 # 1) Install Julia (Juliaup is great): https://julialang.org/downloads/
-# 2) Start Julia & run this script (should show 7 m navigation errors)
-# 3) Modify files in simple_magnav_data folder with your data
+# 2) Install Visual Studio Code + Julia extension (recommended IDE)
+# 3) Start Julia & run this script (should show 7 m navigation errors)
+# 4) Modify files in simple_magnav_data folder with your data
     # fields for traj (aka GPS) & ins structs:
-        # tt      [s] - time stamps
-        # lat     [deg]
-        # lon     [deg]
-        # alt     [m]
-        # roll    [deg]
-        # pitch   [deg]
-        # yaw     [deg]
-        # mag_1_c [nT] - compensated mag (only in traj)
+        # tt       [s]    time stamps
+        # lat      [deg]  latitude
+        # lon      [deg]  longitude
+        # alt      [m]    altitude
+        # roll     [deg]  roll
+        # pitch    [deg]  pitch
+        # yaw      [deg]  yaw
+        # mag_1_c  [nT]   scalar magnetic data (only in traj)
     # fields for map struct:
-        # map [nT]  - shape is yy by xx
-        # xx  [deg] - longitude here
-        # yy  [deg] - latitude here
-        # alt [m]
-# 4) Modify filter parameters in this script for your data
-# 5) Re-run this script
+        # map  [nT]   magnetic anomaly map (shape of yy by xx)
+        # xx   [deg]  longitude
+        # yy   [deg]  latitude
+        # alt  [m]    altitude (assumed single constant HAE)
+# 5) Modify filter parameters in this script for your data
+# 6) Re-run this script
+# 7) Have FUN and/or frustration
 
 ##* Setup
 cd(@__DIR__)
@@ -27,32 +29,32 @@ using MagNav
 using Random: seed!
 dir = "simple_magnav_data";
 
-##* Load data from CSV files
+##* Option 1: load data from CSV files
 seed!(33); # for reproducibility
 xyz_csv = "$dir/xyz.csv";
-map_csv = "$dir/map"; # folder with alt, map, xx, yy
+map_csv = "$dir/map"; # folder with alt, map, xx, & yy CSV files
 xyz1    = get_XYZ0(xyz_csv);
 mapS1   = get_map(map_csv);
 
-##* Load data from HDF5 files
+##* Option 2: load data from HDF5 files
 seed!(33); # for reproducibility
-xyz_h5  = "$dir/xyz.h5"
-map_h5  = "$dir/map.h5"
+xyz_h5  = "$dir/xyz.h5";
+map_h5  = "$dir/map.h5";
 xyz2    = get_XYZ0(xyz_h5);
 mapS2   = get_map(map_h5);
 
-##* Load data from MAT files
+##* Option 3: load data from MAT files
 seed!(33); # for reproducibility
 xyz_mat = "$dir/xyz.mat";
 map_mat = "$dir/map.mat";
 xyz3    = get_XYZ0(xyz_mat);
 mapS3   = get_map(map_mat);
 
-##* Choose data from one of above load options
+##* Choose data from load option 1, 2, or 3 above (HDF5 selected here)
 xyz  = xyz2;
 mapS = mapS2;
 
-##* Create, run, and plot navigation filter
+##* Create navigation model
 (P0,Qd,R) = create_model(xyz.traj.dt, xyz.traj.lat[1];
                          init_pos_sigma = 3.0,           # initial position uncertainty [m]
                          init_alt_sigma = 0.001,         # initial altitude uncertainty [m]
@@ -72,17 +74,18 @@ mapS = mapS2;
                          gyro_tau       = 3600.0,        # gyroscope time constant [s]
                          fogm_tau       = 600.0);        # FOGM catch-all time constant [s]
 
+##* Run navigation filter
 itp_mapS = map_interpolate(mapS); # map interpolation function
-
 (crlb_out,ins_out,filt_out) = run_filt(xyz.traj, xyz.ins, xyz.mag_1_c, itp_mapS, :ekf;
-                                       P0       = P0,
-                                       Qd       = Qd,
-                                       R        = R,
-                                       baro_tau = 3600.0,
-                                       acc_tau  = 3600.0,
-                                       gyro_tau = 3600.0,
-                                       fogm_tau = 600.0,
-                                       date     = get_years(2024,25), # f(year, day of year), only needed if core = true
-                                       core     = false);             # core field contained within xyz.mag_1_c field
+                                       P0       = P0,     # initial covariance matrix
+                                       Qd       = Qd,     # discrete time process/system noise matrix
+                                       R        = R,      # measurement (white) noise variance
+                                       baro_tau = 3600.0, # barometer time constant [s]
+                                       acc_tau  = 3600.0, # accelerometer time constant [s]
+                                       gyro_tau = 3600.0, # gyroscope time constant [s]
+                                       fogm_tau = 600.0,  # FOGM catch-all time constant [s]
+                                       date     = get_years(2025,1), # measurement date (decimal year), for core = true
+                                       core     = false);            # if core field contained within xyz.mag_1_c field
 
+##* Plot navigation results
 p1 = plot_filt(xyz.traj,xyz.ins,filt_out;show_plot=false)[1]
