b2e92234110da328848b522306f8b8f5a90221ee
Albert Gnadt <20693045+gnadt@users.noreply.github.com>
2025-06-30 22:30:36 -0400

    Bump MagNav version in Pluto examples

--- examples/pluto_fi.jl
+++ examples/pluto_fi.jl
@@ -164,7 +164,7 @@ Statistics = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 [compat]
 CSV = "~0.10.15"
 DataFrames = "~1.7.0"
-MagNav = "~1.3.2"
+MagNav = "~1.3.3"
 Plots = "~1.40.14"
 Random = "~1.11.0"
 Statistics = "~1.11.1"
