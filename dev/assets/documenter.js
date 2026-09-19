ebc4747e9aed63ea66f3d2c41e2b6c51987cebe5
Documenter.jl <documenter@juliadocs.github.io>
2026-09-19 17:46:58 +0000

    build based on 3f3ccb6

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -10,15 +10,15 @@ requirejs.config({
     'highlight-julia-repl': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/julia-repl.min',
   },
   shim: {
-  "highlight-julia": {
-    "deps": [
-      "highlight"
-    ]
-  },
   "headroom-jquery": {
     "deps": [
       "jquery",
       "headroom"
+    ]
+  },
+  "highlight-julia": {
+    "deps": [
+      "highlight"
     ]
   },
   "highlight-julia-repl": {
@@ -26,19 +26,15 @@ requirejs.config({
       "highlight"
     ]
   }
-}
-});
+}});
 ////////////////////////////////////////////////////////////////////////////////
 require([], function() {
 window.MathJax = {
+  "options": {
+    "ignoreHtmlClass": "tex2jax_ignore",
+    "processHtmlClass": "tex2jax_process"
+  },
   "tex": {
-    "tags": "ams",
-    "packages": [
-      "base",
-      "ams",
-      "autoload",
-      "mhchem"
-    ],
     "inlineMath": [
       [
         "$",
@@ -48,14 +44,16 @@ window.MathJax = {
         "\\(",
         "\\)"
       ]
-    ]
-  },
-  "options": {
-    "processHtmlClass": "tex2jax_process",
-    "ignoreHtmlClass": "tex2jax_ignore"
+    ],
+    "packages": [
+      "base",
+      "ams",
+      "autoload",
+      "mhchem"
+    ],
+    "tags": "ams"
   }
-}
-;
+};
 
 (function () {
     var script = document.createElement('script');
